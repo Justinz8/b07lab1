e80bdc3b5782ea37a3428b776fad3cfbdf0d055d
Justinz8 <Justinzhang9001@gmail.com>
2024-09-26 17:32:15 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,29 @@
+import java.io.*;
+
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+	public static void main(String [] args) {
+		try {
+		Polynomial p = new Polynomial();
+		System.out.println(p.evaluate(3));
+		double [] c1 = {6,0,0,5};
+		int [] d1 = {0,1,2,3};
+		Polynomial p1 = new Polynomial(c1, d1);
+		Polynomial p2 = new Polynomial(new File("./input.txt"));
+		Polynomial s = p1.add(p2);
+		
+		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		if(s.hasRoot(1))
+			System.out.println("1 is a root of s");
+		else
+			System.out.println("1 is not a root of s");
+		
+		new File("./test.txt");
+		
+		double[] c2 = {1, 2, 3};
+		int[] d2 = {0, 1, 2};
+		Polynomial p3 = new Polynomial(c2, d2);
+		
+		s.multiply(p3).saveToFile("./test.txt");
+		} catch (Exception ex) {System.out.println(ex);}
+	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,26 +1,135 @@
+import java.io.*;
 import java.util.*;
 
 public class Polynomial {
-	double equation[];
+	double coefficient[];
+	int degree[];
 	
 	public Polynomial() {
-		equation = new double[1];
+		coefficient = new double[0];
+		degree = new int[0];
 	}
-	public Polynomial(double current[]) {
-		equation = current;
+	public Polynomial(double coefficient[], int degree[]) {
+		this.coefficient = coefficient.clone();
+		this.degree = degree.clone();
 	}
-	public Polynomial add(Polynomial poly2) {
-		double newPoly[] = new double[Math.max(poly2.equation.length, equation.length)];
-		for(int i = 0; i<newPoly.length; i++) {
-			if(i<poly2.equation.length) newPoly[i]+=poly2.equation[i];
-			if(i<equation.length) newPoly[i]+=equation[i];
+	public Polynomial(File file) throws IOException {
+		Scanner sc = new Scanner(file);
+		
+		String poly = sc.nextLine();
+		String parsedPoly[] = (poly.charAt(0)+poly.substring(1).replace("-", "+-")).split("\\+");
+		int polysize = parsedPoly.length;
+		
+		int newDegree[] = new int[polysize];
+		double newCoeff[] = new double[polysize];
+		
+		for(int i = 0; i<polysize; i++) {
+			String parsedTerm[] = parsedPoly[i].split("x");
+			if(parsedTerm.length == 1) {
+				newDegree[i] = 0;
+				if(parsedPoly[i].contains("x")) newDegree[i] = 1;
+				newCoeff[i] = Double.parseDouble(parsedTerm[0]);
+			}else {
+				newDegree[i] = Integer.parseInt(parsedTerm[1]);
+				newCoeff[i] = Double.parseDouble(parsedTerm[0]);
+			}
 		}
-		return new Polynomial(newPoly);
+		
+		degree = newDegree;
+		coefficient = newCoeff;
 	}
+	
+	public void saveToFile(String fileName) throws FileNotFoundException {
+		PrintStream output = new PrintStream(fileName);
+		for(int i = 0; i<coefficient.length; i++) {
+			if(coefficient[i]==0) continue;
+			if(coefficient[i]>0 && i!=0) output.append('+');
+			if(coefficient[i] == (int) coefficient[i]) {
+				output.append(((int) coefficient[i])+"");
+			}else {
+				output.append(coefficient[i]+"");
+			}
+			if(degree[i]==0) continue;
+			output.append('x');
+			if(degree[i]==1) continue;
+			output.append(degree[i]+"");
+		}
+	}
+	
+	public Polynomial add(Polynomial poly2) {
+		
+		int maxDegree = 0;//largest degree between both polys
+		
+		for(int i:degree) maxDegree = Math.max(maxDegree, i);
+		for(int i:poly2.degree) maxDegree = Math.max(maxDegree, i);
+		
+		double tmpPoly[] = new double[maxDegree+1];
+		
+		int PolyLength = 0; //track how many distinct degrees there are
+		
+		for(int i = 0; i<degree.length; i++) {
+			if(tmpPoly[degree[i]]==0 && coefficient[i]!=0) PolyLength++;
+			tmpPoly[degree[i]]+=coefficient[i]; //add coefficient to this degree
+		}
+		
+		for(int i = 0; i<poly2.degree.length; i++) {
+			if(tmpPoly[poly2.degree[i]]==0 && poly2.coefficient[i]!=0) PolyLength++;
+			tmpPoly[poly2.degree[i]]+=poly2.coefficient[i];
+		}
+		
+		double newCoeff[] = new double[PolyLength];
+		int newDegree[] = new int[PolyLength];
+		
+		int idx = 0;
+		
+		for(int i = 0; i<maxDegree+1; i++) {
+			if(tmpPoly[i]!=0) { //if coefficient for this degree is 0 then skip
+				newCoeff[idx] = tmpPoly[i];
+				newDegree[idx] = i;
+				idx++;
+			}
+		}
+		
+		return new Polynomial(newCoeff, newDegree);
+	}
+	
+	public Polynomial multiply(Polynomial poly2) {
+		int maxDegree = 0;
+		for(int i = 0; i<degree.length; i++) {
+			for(int j = 0; j<poly2.degree.length; j++) {
+				maxDegree = Math.max(maxDegree, degree[i]+poly2.degree[j]); //find max degree from multiplying
+			}
+		}
+		double tmpPoly[] = new double[maxDegree+1];
+		
+		int polyLength = 0; //track number of distinct degrees
+		
+		for(int i = 0; i<degree.length; i++) {
+			for(int j = 0; j<poly2.degree.length; j++) { //if coefficient for this degree is 0 then skip
+				int newD = degree[i]+poly2.degree[j];
+				if(tmpPoly[newD]==0 && coefficient[i]*poly2.coefficient[j]!=0) polyLength++;//if distinct degree then increment count
+				tmpPoly[newD] += coefficient[i]*poly2.coefficient[j];
+			}
+		}
+		
+		int newDegree[] = new int[polyLength];
+		double newCoefficient[] = new double[polyLength];
+		int idx = 0;
+		
+		for(int i = 0; i<maxDegree+1; i++) {
+			if(tmpPoly[i]!=0) {
+				newDegree[idx] = i;
+				newCoefficient[idx] = tmpPoly[i];
+				idx++;
+			}
+		}
+		return new Polynomial(newCoefficient, newDegree);
+	}
+	
 	public double evaluate(double n) {
 		double ans = 0;
-		for(int i = 0; i<equation.length; i++) {
-			ans+=equation[i]*Math.pow(n, i);
+		for(int i = 0; i<coefficient.length; i++) {
+			ans+=coefficient[i]*Math.pow(n, degree[i]);
 		}
 		return ans;
 	}
